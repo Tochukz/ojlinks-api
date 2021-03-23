31e31c133473a85cebecb002b5d23f1e1fc6c9f7
Tochukwu Nwachukwu <truetochukz@gmail.com>
2021-03-23 14:11:44 +0200

    Added Socket Feature

--- models/Category.js
+++ models/Category.js
@@ -3,7 +3,7 @@ const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
 class Category extends Model {
     static associate(models) {    
-      models.Category.hasMany(models.Subcategory, { as: 'Subcategories', foreignKey: 'categoryId', sourceKey: 'categoryId'});
+      models.Category.hasMany(models.Subcategory, { foreignKey: 'categoryId', sourceKey: 'categoryId', as: 'subcategories'});
     }
   };
   Category.init({
--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,16 @@ const { Category, Subcategory, Book } = require('../models');
 
 router.get('/categories', async (req, res, next) => {
   try {
-    const categories = await Category.findAll({include: [{model: Subcategory, as: 'Subcategories'}]});
+    const categories = await Category.findAll({
+      attributes: ['categoryId', 'name'],
+      include: [
+        {
+          model: Subcategory, 
+          as: 'subcategories',
+          attributes: ['subcategoryId', 'name']
+        }
+      ]
+    });
     const sub = await categories[0].getSubcategories();
     res.send(categories);
   } catch(err) {
@@ -16,7 +25,19 @@ router.get('/categories', async (req, res, next) => {
 router.get('/categories/:categoryId', async (req, res, next) => {
   try {
     const categoryId = req.params.categoryId;
-    const category = await Category.findOne({ where: { categoryId }, include: [{model: Subcategory, as: 'Subcategories'}]});
+    const category = await Category.findOne({ 
+      where: { 
+        categoryId 
+      }, 
+      attributes: ['categoryId', 'name'],
+      include: [
+        {
+          model: Subcategory, 
+          as: 'subcategories',
+          attributes: ['subcategoryId', 'name']
+        }
+      ]
+    });
     res.send(category);
   } catch(err) {
     return next(err);
@@ -25,7 +46,21 @@ router.get('/categories/:categoryId', async (req, res, next) => {
 
 router.get('/subcategories', async (req, res, next) => {
   try {
-    const subcategories = await Subcategory.findAll({include: [{model: Category, as: 'Category'}, {model: Book, as: 'Books'}]});
+    const subcategories = await Subcategory.findAll({
+      include: [
+        {
+          model: Category, 
+          as: 'category',
+          attributes: ['categoryId', 'name'],
+        }, 
+        {
+          model: Book, 
+          as: 'books',
+          attributes: ['bookId', 'title', 'author', 'price', 'edition', 'availability', 'img']
+        }
+      ],
+      attributes: ['subcategoryId', 'name'],
+    });
     res.send(subcategories);
   } catch(err) {
     return next(err);
@@ -35,7 +70,23 @@ router.get('/subcategories', async (req, res, next) => {
 router.get('/subcategories/:subcategoryId', async (req, res, next) => {
   try {
     const subcategoryId = req.params.subcategoryId;
-    const subcategories = await Subcategory.findOne({where: { subcategoryId }, include: [{model: Category, as: 'Category'}, {model: Book, as: 'Books'}]});
+    const subcategories = await Subcategory.findOne({
+      where: { 
+        subcategoryId 
+      }, 
+      include: [
+        {
+          model: Category, 
+          as: 'category',
+          attributes: ['categoryId', 'name'],
+        }, 
+        {
+          model: Book, 
+          as: 'books',
+          attributes: ['bookId', 'title', 'author', 'price', 'edition', 'availability', 'img']
+        }
+      ]
+    });
     res.send(subcategories);
   } catch(err) {
     return next(err);
@@ -44,7 +95,15 @@ router.get('/subcategories/:subcategoryId', async (req, res, next) => {
 
 router.get('/books', async (req, res, next) => {
   try {
-    const books = await Book.findAll({include: [{model: Subcategory, as: 'Subcategory'}]});
+    const books = await Book.findAll({
+      include: [
+        {
+          model: Subcategory, 
+          as: 'subcategory',
+          attributes: ['subcategoryId', 'name'],
+        }
+      ]
+    });
     res.send(books);
   } catch(err) {
     return next(err);
@@ -54,11 +113,30 @@ router.get('/books', async (req, res, next) => {
 router.get('/books/:bookId', async (req, res, next) => {
   try {
     const bookId = req.params.bookId;
-    const books = await Book.findOne({where: { bookId }, include: [{model: Subcategory, as: 'Subcategory'}]});
+    const books = await Book.findOne({
+      where: { 
+        bookId 
+      }, 
+      include: [
+        {
+          model: Subcategory, 
+          as: 'subcategory',
+          attributes: ['subcategoryId', 'name'],
+        }
+      ]
+    });
     res.send(books);
   } catch(err) {
     return next(err);
   }
 });
+
+router.post('/order/create', async(req, res, next) => {
+  try {
+    return res.json(req.body);
+  } catch(err) {
+    return next(err);
+  }
+})
 
 module.exports = router;
